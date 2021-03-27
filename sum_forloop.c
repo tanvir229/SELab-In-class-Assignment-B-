a8a56ebb31683a8f6779bf0ec493af7264c267b6
tanvir229 <40928557+tanvir229@users.noreply.github.com>
2021-03-27 12:44:45 +0600

    Update sum_forloop.c

--- sum_forloop.c
+++ sum_forloop.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
+int main() {
+    int i,j,k,n=3;
+    int arr[] = {1, 3, 5};
+    printf("sum of the elements of this array is %d", sum(arr, n));
+    return 0;
+}
+
 int sum(int arr[], int n) {
     int s = 0;
     int i;
@@ -9,19 +16,11 @@ int sum(int arr[], int n) {
     return s;
 }
 
-
-int main() {
-    int arr[100];
-    int i,j,k,n;
-
-    printf("Enter size of array ");
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-        printf("%dth element ",i+1);
-        scanf("%d",&arr[i]);
-    }
-
-    printf("sum of the elements of this array is %d", sum(arr, n));
+int ret_z(){
     return 0;
 }
+
+void hello(){
+    printf("hello");
+}
+
